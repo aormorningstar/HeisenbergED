bf13c7047b35e95fbfbc5f692f3103d80710229b
aormorningstar <aormorningstar@Alans-MacBook-Pro.local>
2017-06-11 11:53:30 -0400

    fixed transpose error

--- sparseHam.jl
+++ sparseHam.jl
@@ -31,9 +31,12 @@ function constructSparseHam(basis::SzkxkyBasis,c::couplings,s::sector,l::lattice
     Knz::Bool = (K != 0.0);
 
     # store location and value of non-zero matrix elements
-    I::Array{Int32} = Int32[];
-    J::Array{Int32} = Int32[];
-    M::Array{Complex128} = Complex128[];
+    I::Array{Int32,1} = Int32[];
+    J::Array{Int32,1} = Int32[];
+    M::Array{Complex128,1} = Complex128[];
+
+    # allocate memory once before loop
+
 
     # loop over basis
     for bIndex::Int64 in 1:basis.dim
@@ -94,6 +97,10 @@ function constructSparseHam(basis::SzkxkyBasis,c::couplings,s::sector,l::lattice
                 a12repIndex::Int32 = basisIndex(a12rep,basis);
                 # the matrix element
                 Hsite12::Complex128 = 0.5*J1+0.125*K*(sPw34-sPw1234+2*sPw1D2D)*exp(-1.0im*(kx*lx12+ky*ly12))*sqrt(basis.n[a12repIndex]/nb);
+
+                push!(J,bIndex);
+                push!(I,a12repIndex);
+                push!(M,Hsite12);
             end;
 
             # the 13 term
@@ -106,6 +113,10 @@ function constructSparseHam(basis::SzkxkyBasis,c::couplings,s::sector,l::lattice
                 a13repIndex::Int32 = basisIndex(a13rep,basis);
                 # the matrix element
                 Hsite13::Complex128 = 0.5*J1*+0.125*K*(sPw24-sPw1234+2*sPw1L3L)*exp(-1.0im*(kx*lx13+ky*ly13))*sqrt(basis.n[a13repIndex]/nb);
+
+                push!(J,bIndex);
+                push!(I,a13repIndex);
+                push!(M,Hsite13);
             end;
 
             # the 14 term
@@ -118,6 +129,10 @@ function constructSparseHam(basis::SzkxkyBasis,c::couplings,s::sector,l::lattice
                 a14repIndex::Int32 = basisIndex(a14rep,basis);
                 # the matrix element
                 Hsite14::Complex128 = -0.250*K*sPw23*exp(-1.0im*(kx*lx14+ky*ly14))*sqrt(basis.n[a14repIndex]/nb);
+
+                push!(J,bIndex);
+                push!(I,a14repIndex);
+                push!(M,Hsite14);
             end;
 
             # the 23 term
@@ -130,6 +145,10 @@ function constructSparseHam(basis::SzkxkyBasis,c::couplings,s::sector,l::lattice
                 a23repIndex::Int32 = basisIndex(a23rep,basis);
                 # the matrix element
                 Hsite23::Complex128 = -0.25*K*sPw14*exp(-1.0im*(kx*lx23+ky*ly23))*sqrt(basis.n[a23repIndex]/nb);
+
+                push!(J,bIndex);
+                push!(I,a23repIndex);
+                push!(M,Hsite23);
             end;
 
             # the 1234 term
@@ -142,51 +161,17 @@ function constructSparseHam(basis::SzkxkyBasis,c::couplings,s::sector,l::lattice
                 a1234repIndex::Int32 = basisIndex(a1234rep,basis);
                 # the matrix element
                 Hsite1234::Complex128 = 0.125*K*(2-sPw12-sPw34-sPw13-sPw24+sPw14+sPw23)*exp(-1.0im*(kx*lx1234+ky*ly1234))*sqrt(basis.n[a1234repIndex]/nb);
-            end;
 
-            # push the matrix elements and locations seperately
-            # -------------------------------------------------
-
-            # the 12 term
-            if c12
-                push!(I,bIndex);
-                push!(J,a12repIndex);
-                push!(M,Hsite12);
-            end;
-
-            # the 13 term
-            if c13
-                push!(I,bIndex);
-                push!(J,a13repIndex);
-                push!(M,Hsite13);
-            end;
-
-            # the 14 term
-            if c14
-                push!(I,bIndex);
-                push!(J,a14repIndex);
-                push!(M,Hsite14);
-            end;
-
-            # the 23 term
-            if c23
-                push!(I,bIndex);
-                push!(J,a23repIndex);
-                push!(M,Hsite23);
-            end;
-
-            # the 1234 term
-            if c1234
-                push!(I,bIndex);
-                push!(J,a1234repIndex);
+                push!(J,bIndex);
+                push!(I,a1234repIndex);
                 push!(M,Hsite1234);
             end;
 
         end;
 
         # push diagonal matrix element to list of matrix elements
-        push!(I,bIndex);
         push!(J,bIndex);
+        push!(I,bIndex);
         push!(M,Hbb);
 
     end;
