daaf47967b223299553d638cbce4416ba81db676
aormorningstar <aormorningstar@Alans-MacBook-Pro.local>
2017-06-29 10:11:29 -0400

    replaced building S^2 with on-the-fly in-place multiplication

--- sparseS2.jl
+++ sparseS2.jl
@@ -109,3 +109,91 @@ function constructSparseS2(basis::SzkxkyBasis,s::sector,l::lattice)
 
     return S2;
 end;
+
+
+# function for in place multiplication by the S^2 operator
+function S2_mul_psi!(basis::SzkxkyBasis,s::sector,l::lattice,S2psi::Vector{Complex128},psi::Vector{Complex128})
+
+    # clear output Vector
+    S2psi .= 0.0+0.0im;
+
+    # lattice
+    Lx::Int64 = l.Lx;
+    Ly::Int64 = l.Ly;
+    N::Int64 = l.N;
+    # momentum
+    kx::Float64 = s.kx;
+    ky::Float64 = s.ky;
+
+    # allocate memory once before the loops
+    # -------------------------------------
+    b::UInt64 = 0;
+    sb::Array{UInt64,1} = Array{UInt64,1}(N);
+    nb::Float64 = 0.0;
+    S2bb::Complex128 = 0.0+0.0im;
+    sPwij::Array{Int64,2} = Array{Int64,2}(N,N);
+    a::UInt64 = UInt64(0);
+    aRep::UInt64 = UInt64(0);
+    lx::Int64 = ly::Int64 = 0;
+    aRepIndex::Int32 = Int32(0);
+    S2ij::Complex128 = 0.0+0.0im;
+    # -------------------------------------
+
+    # loop over basis
+    for bIndex::Int64 in 1:basis.dim
+
+        # spin states of basis
+        b = basis.b[bIndex]; # integer rep.
+        for bit in 1:l.N
+            sb[bit] = readBit(b,bit); # spin array rep.
+        end;
+
+        # normalization constant
+        nb = basis.n[bIndex];
+
+        # fill the sPwij table
+        for i::Int64 in 1:N
+            for j::Int64 in 1:(i-1)
+                sPwij[i,j] = simplePower(sb[i]+sb[j]);
+            end;
+        end;
+
+        # initialize the diagonal matrix element
+        S2bb = 0.75*N + 0.0im;
+
+        # loop over pairs of lattice sites
+        for i::Int64 in 1:N
+            for j::Int64 in 1:(i-1)
+
+                # contribute to the diagonal matrix element
+                # -----------------------------------------
+                S2bb += 0.5*sPwij[i,j];
+
+                # compute off diagonal matrix elements
+                # ------------------------------------
+
+                if sb[i] != sb[j]
+                    # the bra
+                    a = XiXj(b,i,j);
+                    # the rep and translation of the bra
+                    aRep,lx,ly = representative(a,l);
+                    # search for this rep in the basis
+                    aRepIndex = basisIndex(aRep,basis);
+                    if aRepIndex != 0
+                        # the matrix element
+                        S2ij = 0.5*(1-sPwij[i,j])*exp(-1.0im*(kx*lx+ky*ly))*sqrt(basis.n[aRepIndex]/nb);
+                        # contributing to the output vector
+                        S2psi[aRepIndex] += S2ij*psi[bIndex];
+                    end;
+                end;
+
+            end;
+        end;
+
+        # diagonal element contribution to output
+        S2psi[bIndex] += S2bb*psi[bIndex];
+
+    end;
+
+    return S2psi::Vector{Complex128};
+end;
